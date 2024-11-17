1681d9774ac0f56590e0935ac924b455cc431610
RinatStr102 <tamam-102@mail.ru>
2024-11-17 23:23:02 +0300

    Добавлены build.gradle configuration

---
+++ src/test/java/com/rinat/filehandling/ZipFileTest.java
@@ -0,0 +1,12 @@
+package com.rinat.filehandling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ZipFileTest {
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
